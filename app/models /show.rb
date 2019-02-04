aaaf96aea5a848c4b732f91eb00ebf4e041e2716
Joshua D. Forrest <joshuadforrest@gmail.com>
2019-02-04 19:24:24 +0000

    Automatically backed up by Learn

--- app/models /show.rb
+++ app/models /show.rb
@@ -1,6 +1,9 @@
 class Shows < ActiveRecord::Base
-        def highest_rating
-        end
-        def most_popular_show
-        end
+  def highest_rating
+    
+  end
+  def most_popular_show
+    
+  end
+  def 
 end
